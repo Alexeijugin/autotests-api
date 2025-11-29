3ce304ea94591ad4964c8457a188b5fdd927ebd5
Alexeijugin <57283269+Alexeijugin@users.noreply.github.com>
2025-11-29 03:23:12 +0300

    рефакторинг и подготовка к написанию API автотестов

---
+++ fixtures/files.py
@@ -0,0 +1,23 @@
+import pytest
+from pydantic import BaseModel
+
+from clients.files.files_client import get_files_client, FilesClient
+from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema
+from fixtures.users import UserFixture
+
+
+class FileFixture(BaseModel):
+    request: CreateFileRequestSchema
+    response: CreateFileResponseSchema
+
+
+@pytest.fixture
+def file_client(function_user: UserFixture) -> FilesClient:
+    return get_files_client(function_user.authentication_user)
+
+
+@pytest.fixture
+def function_files(file_client) -> FileFixture:
+    request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+    response = file_client.create_file(request)
+    return FileFixture(request=request, response=response)
